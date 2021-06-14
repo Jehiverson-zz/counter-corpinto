2d34d3aba3fd9636c1e302a92174538c798fa0cd
Jehiverson <jehivis@gmail.com>
2021-06-14 12:29:38 -0600

    first commit

--- routes/index.js
+++ routes/index.js
@@ -6,22 +6,9 @@ const User = require('../models/User');
 
 router.post("/counter", async(req, res) => {
     if (req.body.data.measurements) {
-        const { from, to, items } = req.body.data.measurements[0];
-        const { name } = req.body.sensor;
+        
+        console.log('---------PUTA SI LLEGO---------',req.body.data.measurements);
 
-        const datosImport = {
-            from: from,
-            to: to,
-            in: items[0].count,
-            adultIn: items[0].adults,
-            out: items[1].count,
-            adultOut: items[1].adults,
-            store: name,
-        };
-        
-        let docName = datosImport.store+''+datosImport.to;
-        console.log('------------------',datosImport);
-        
     } else {
         console.log("measurements vacio");
     }
